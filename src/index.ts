d1c26df03044e1593bd2e951df7c1839bfa4e0e8
Matt Rayner <me@mattihew.com>
2019-07-16 20:49:05 +0100

    added eslint to package scripts

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,6 @@
-import {check} from "k6";
+import { check } from "k6";
 import * as http from "k6/http";
-import {Options} from "k6/options";
+import { Options } from "k6/options";
 import createForm from "./anotherModule.js";
 
 export const options: Partial<Options> = {
@@ -8,10 +8,10 @@ export const options: Partial<Options> = {
     iterations: 20
 };
 
-export default function(): void {
+export default function (): void {
     const res = http.get(`http://jsonplaceholder.typicode.com/users/${__VU}`);
     check(res, {
         "is status 200": r => r.status === 200
     });
-    console.log(createForm());
+    console.log("createForm: " + createForm());
 }
